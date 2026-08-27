e269520ba52d767031cead369a8a1751ae64f6fb
Mohammed Salih k <salihsha656@gmail.com>
2026-08-27 15:35:36 +0530

    fix eslint rules, build passing

--- frontend/lib/actions/user.ts
+++ frontend/lib/actions/user.ts
@@ -6,7 +6,7 @@ export async function getCurrentUser() {
 
   if (!user) return null
 
-  const { data: profile,error } = await supabase
+  const { data: profile } = await supabase
     .from('profiles')
     .select('*')
     .eq('id', user.id)
